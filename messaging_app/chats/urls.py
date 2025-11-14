f5646b8a644775d0435b01140c550e2f09d84e84
ifedips20s <ifedips20s@gmail.com>
2025-11-14 02:09:55 +0100

    Initial Django project setup with DRF and chats app

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
+# Create a DRF router
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversations')
 router.register(r'messages', MessageViewSet, basename='messages')
 
+# Include router URLs
 urlpatterns = [
-    path('', include(router.urls)),  # include all router endpoints
+    path('', include(router.urls)),
 ]
