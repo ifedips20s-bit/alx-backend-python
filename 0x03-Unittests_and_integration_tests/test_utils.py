3684814c2c5114aebbf53847a6ab16b3fd674168
ifedips20s <ifedips20s@gmail.com>
2025-11-15 22:47:38 +0100

    Complete unit tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -67,7 +67,9 @@ class TestMemoize(unittest.TestCase):
 
         test_obj = TestClass()
 
-        with patch.object(test_obj, "a_method", return_value=42) as mock_method:
+        with patch.object(
+            test_obj, "a_method", return_value=42
+        ) as mock_method:
             result1 = test_obj.a_property
             result2 = test_obj.a_property
 
