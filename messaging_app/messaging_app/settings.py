2463d2c347728afe06c0c2cb81c79ba6ed643cb7
ifedips20s <ifedips20s@gmail.com>
2025-11-18 12:10:50 +0100

    Add auth file

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -17,7 +17,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'messaging_app.chats.permissions.IsParticipantOfConversation',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'messaging_app.chats.pagination.MessagePagination',
+    'DEFAULT_PAGINATION_CLASS': 'messaging_app.chats.pagination.MessagePagination',  # uses PageNumberPagination internally
     'PAGE_SIZE': 20,
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
