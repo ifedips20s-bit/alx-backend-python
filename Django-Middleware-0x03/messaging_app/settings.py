3bb10cae05e1af4c117c2cb02f7890e6d83796fd
ifedips20s <ifedips20s@gmail.com>
2025-11-21 18:31:13 +0100

    Set up Django-Middleware-0x03 project and added logging middleware

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -49,6 +49,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+     # Custom middleware
+    'chats.middleware.RequestLoggingMiddleware',
 ]
 
 
